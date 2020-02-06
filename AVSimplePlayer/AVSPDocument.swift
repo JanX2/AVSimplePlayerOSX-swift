951f1c3f7f9fc64fb00fc514bfca004bac0c1a1e
Jan <github@geheimwerk.de>
2020-02-06 20:14:00 +0100

    Swift port.

---
+++ AVSimplePlayer/AVSPDocument.swift
@@ -0,0 +1,344 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2020 Jan Weiß http://geheimwerk.de/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+/*
+     File: AVSPDocument.m
+ Abstract: The players document class. It sets up the AVPlayer, AVPlayerLayer, manages adjusting the playback rate, enables and disables UI elements as appropriate, sets up a time observer for updating the current time (which the UI's time slider is bound to), and handles adjusting the volume of the AVPlayer.
+  Version: 1.1.1-Swift
+ */
+
+import Cocoa
+import AVFoundation
+//#import "AVSPDocument.h"
+//#import <AVFoundation/AVFoundation.h>
+
+
+class AVSPDocument: NSDocument {
+
+	private var rateObserver: NSKeyValueObservation? = nil
+	private var statusObserver: NSKeyValueObservation? = nil
+	private var readyForDisplayObserver: NSKeyValueObservation? = nil
+	
+	@objc private var player: AVPlayer? = nil
+    private var playerLayer: AVPlayerLayer? = nil
+	
+	
+	@objc dynamic var currentTime: Double {
+		get {
+			guard let player = player else {
+				return 0.0
+			}
+			
+			return CMTimeGetSeconds(player.currentTime())
+		}
+		set {
+			guard let player = player else {
+				return
+			}
+			
+			let timeScale: CMTimeScale = player.currentItem?.duration.timescale ?? 1000
+			player.seek(to: CMTimeMakeWithSeconds(newValue, preferredTimescale: timeScale), toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+		}
+	}
+	
+	
+    @objc dynamic var duration: Double {
+		get {
+			guard let player = player else {
+				return 0.0
+			}
+			
+			let playerItem: AVPlayerItem! = player.currentItem
+
+			if playerItem.status == .readyToPlay {
+				return CMTimeGetSeconds(playerItem.asset.duration)
+			}
+			else {
+				return 0.0
+			}
+		}
+	}
+	
+	@objc class func keyPathsForValuesAffectingDuration() -> Set<String> {
+		return Set(["player.currentItem", "player.currentItem.status"])
+    }
+	
+	
+	@objc dynamic var volume: Float{
+		get {
+			guard let player = self.player else {
+				return 0.0
+			}
+			
+			return player.volume
+		}
+		set {
+			guard let player = player else {
+				return
+			}
+			
+			player.volume = newValue
+		}
+	}
+	
+	@objc class func keyPathsForValuesAffectingVolume() -> Set<String> {
+		return Set(["player.volume"])
+	}
+	
+    @IBOutlet private var loadingSpinner: NSProgressIndicator? = nil
+    @IBOutlet private var unplayableLabel: NSTextField? = nil
+    @IBOutlet private var noVideoLabel: NSTextField? = nil
+    @IBOutlet private var playerView: NSView? = nil
+    @IBOutlet private var playPauseButton: NSButton? = nil
+    @IBOutlet private var fastForwardButton: NSButton? = nil
+    @IBOutlet private var rewindButton: NSButton? = nil
+    @IBOutlet private var timeSlider: NSSlider? = nil
+	
+    private var timeObserverToken: Any? = nil
+	
+	
+	override var windowNibName: NSNib.Name? {
+        return NSNib.Name("AVSPDocument")
+    }
+
+	override func windowControllerDidLoadNib(_ windowController: NSWindowController) {
+        super.windowControllerDidLoadNib(windowController)
+		
+		windowController.window?.isMovableByWindowBackground = true
+		self.playerView?.layer?.backgroundColor = CGColor.black
+		self.loadingSpinner?.startAnimation(self)
+
+    	// Create the AVPlayer, add rate and status observers
+    	self.player = AVPlayer()
+		guard let player = self.player else {
+			return
+		}
+		
+		self.rateObserver = player.observe(\.rate, options: [.new]) {
+			(playerItem, change) in
+			guard let newValue = change.newValue else {
+				return
+			}
+			
+			let rate: Float = newValue
+			if rate != 1.0 {
+				self.playPauseButton?.title = "Play"
+			}
+			else {
+				self.playPauseButton?.title = "Pause"
+			}
+		}
+		
+		self.statusObserver = player.observe(\.currentItem?.status, options: [.new]) {
+			(player, change) in
+			guard let newValue = change.newValue else {
+				return
+			}
+			
+			let status = newValue
+			var enable = false
+			switch (status) {
+			case .unknown:
+				break
+			case .readyToPlay:
+				enable = true
+				break
+			case .failed:
+				self.stopLoadingAnimationAndHandleError(self.player?.currentItem?.error)
+				break
+			default:
+				break
+			}
+			
+			self.playPauseButton?.isEnabled = enable
+			self.fastForwardButton?.isEnabled = enable
+			self.rewindButton?.isEnabled = enable
+		}
+		
+    	// Create an asset with our URL, asychronously load its tracks and whether it's playable or protected.
+    	// When that loading is complete, configure a player to play the asset.
+		guard let fileURL = self.fileURL else {
+			return
+		}
+		
+		let asset = AVAsset(url: fileURL)
+    	let assetKeysToLoadAndTest = ["playable", "hasProtectedContent", "tracks"]
+		asset.loadValuesAsynchronously(forKeys: assetKeysToLoadAndTest) {
+    		// The asset invokes its completion handler on an arbitrary queue when loading is complete.
+    		// Because we want to access our AVPlayer in our ensuing set-up, we must dispatch our handler to the main queue.
+			DispatchQueue.main.async {
+				self.setUpPlaybackOfAsset(asset: asset, withKeys: assetKeysToLoadAndTest)
+    		}
+    	}
+    }
+
+    func setUpPlaybackOfAsset(asset: AVAsset, withKeys keys: [String]) {
+    	// This method is called when the AVAsset for our URL has completing the loading of the values of the specified array of keys.
+    	// We set up playback of the asset here.
+
+    	// First test whether the values of each of the keys we need have been successfully loaded.
+    	for key in keys {
+    		var error: NSError? = nil
+
+			if asset.statusOfValue(forKey: key, error: &error) == .failed {
+    			self.stopLoadingAnimationAndHandleError(error)
+    			return
+    		}
+    	 }
+
+    	if !asset.isPlayable || asset.hasProtectedContent {
+    		// We can't play this asset. Show the "Unplayable Asset" label.
+    		self.stopLoadingAnimationAndHandleError(nil)
+			self.unplayableLabel?.isHidden = false
+    		return
+    	}
+
+    	// We can play this asset.
+    	// Set up an AVPlayerLayer according to whether the asset contains video.
+		if asset.tracks(withMediaType: AVMediaType.video).count != 0 {
+			guard let layer = self.playerView?.layer else {
+				return
+			}
+			
+    		// Create an AVPlayerLayer and add it to the player view if there is video, but hide it until it's ready for display
+			let newPlayerLayer = AVPlayerLayer(player: self.player)
+			newPlayerLayer.frame = layer.bounds
+			newPlayerLayer.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+			newPlayerLayer.isHidden = true
+    		layer.addSublayer(newPlayerLayer)
+			
+    		self.playerLayer = newPlayerLayer
+			// CRASH
+			self.readyForDisplayObserver = self.observe(\.playerLayer?.isReadyForDisplay, options: [.new]) {
+				_, change in
+				if let newValue = change.newValue,
+					newValue == true {
+					// The AVPlayerLayer is ready for display. Hide the loading spinner and show it.
+					self.stopLoadingAnimationAndHandleError(nil)
+					self.playerLayer?.isHidden = false
+				}
+			}
+    	}
+    	else {
+    		// This asset has no video tracks. Show the "No Video" label.
+    		self.stopLoadingAnimationAndHandleError(nil)
+			self.noVideoLabel?.isHidden = false
+    	}
+
+    	// Create a new AVPlayerItem and make it our player's current item.
+		let playerItem = AVPlayerItem(asset: asset)
+
+    	// If needed, configure player item here (example: adding outputs, setting text style rules, selecting media options) before associating it with a player
+		self.player?.replaceCurrentItem(with: playerItem)
+
+    	// Use a weak self variable to avoid a retain cycle in the block
+		self.timeObserverToken = self.player?.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 1, timescale: 10), queue: DispatchQueue.main) {
+			[weak self] time in
+			self?.timeSlider?.doubleValue = CMTimeGetSeconds(time)
+    	}
+
+    }
+
+    func stopLoadingAnimationAndHandleError(_ error: Error?) { // handleError:
+		if let loadingSpinner = self.loadingSpinner {
+			loadingSpinner.stopAnimation(self)
+			loadingSpinner.isHidden = true
+		}
+		
+    	if let error = error,
+			let windowForSheet = self.windowForSheet {
+			self.presentError(error,
+							  modalFor: windowForSheet,
+							  delegate: nil,
+							  didPresent: nil,
+							  contextInfo: nil)
+    	}
+    }
+
+	override func close() {
+		if let player = player {
+			player.pause()
+			
+			if let timeObserverToken = self.timeObserverToken {
+				player.removeTimeObserver(timeObserverToken)
+			}
+		}
+		
+    	self.timeObserverToken = nil
+		
+    	super.close()
+    }
+	
+    @IBAction func playPauseToggle(sender: AnyObject!) {
+		guard let player = self.player else {
+			return
+		}
+
+    	if player.rate != 1.0 {
+    		if self.currentTime == self.duration {
+				self.currentTime = 0.0
+			}
+    		player.play()
+    	}
+    	else {
+    		player.pause()
+    	}
+    }
+
+    @IBAction func fastForward(sender: AnyObject!) {
+		guard let player = self.player else {
+			return
+		}
+
+    	if player.rate < 2.0 {
+    		player.rate = 2.0
+    	}
+    	else {
+    		player.rate = player.rate + 2.0
+    	}
+    }
+
+    @IBAction func rewind(sender: AnyObject!) {
+		guard let player = self.player else {
+			return
+		}
+
+    	if player.rate > -2.0 {
+    		player.rate = -2.0
+    	}
+    	else {
+    		player.rate = player.rate - 2.0
+    	}
+    }
+
+	override func data(ofType typeName: String) throws -> Data {
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	}
+
+	override func read(from fileWrapper: FileWrapper, ofType typeName: String) throws {
+        return
+    }
+
+
+}
